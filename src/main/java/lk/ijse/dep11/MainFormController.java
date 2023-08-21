3979ca7bb63f1283613580afb8ebe9d379f0f24f
Sachitha Abesinghe <sachithasandakelumsl@gmail.com>
2023-08-22 02:01:43 +0530

    complte splash screen and navigation to text editor

---
+++ src/main/java/lk/ijse/dep11/MainFormController.java
@@ -0,0 +1,10 @@
+package lk.ijse.dep11;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class MainFormController {
+
+    public AnchorPane root;
+    public Label lblLoading;
+}
